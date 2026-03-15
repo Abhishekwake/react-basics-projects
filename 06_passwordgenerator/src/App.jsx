b7cfde2601c894774127314a9b1be4e4e69e0704
Abhishek Wake <abhishekwake111@gmail.com>
2026-03-15 23:00:55 +0530

    Complete Password Generator Project

--- 06_passwordgenerator/src/App.jsx
+++ 06_passwordgenerator/src/App.jsx
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useCallback,useState,useEffect,useRef } from "react";
 import "./App.css";
 
 function App() {
+ const [length, setLength] = useState(8);
+ const [numberAllowed,setNumberAllowed] = useState(false);
+ const [charAllowed,setCharAllowed] = useState(false);
+ const [password,setPassword] = useState("");
+ const passwordRef = useRef(null);
+
+    const passwordGenerator = useCallback(() => {
+      let pass = "";
+      let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+      if(numberAllowed) str += "0123456789";
+      if(charAllowed) str += "!@#$%^&*-_+=[]{}~`";
+      for(let i =1;i<= length;i++){
+        let char = Math.floor(Math.random() * str.length + 1);
+        pass += str.charAt(char);
+      }
+      setPassword(pass);
+    } ,[length,charAllowed,numberAllowed,setPassword]);
+
+    useEffect(() => {
+      passwordGenerator();
+    }, [length, numberAllowed, charAllowed, passwordGenerator]);
+    
+    const copyPasswordToClipboard = useCallback(() => {
+      window.navigator.clipboard.writeText(password);
+      passwordRef.current?.select();
+      passwordRef.current?.setSelectionRange(0, 999); // Optional: select range
+    }, [password])
 
   return (
     <>
@@ -12,9 +39,13 @@ function App() {
             type="text"
             className="outline-none w-full py-1 px-3 bg-amber-50"
             placeholder="Password"
-            readOnly
+            readOnly 
+            value={password}
+            ref={passwordRef}
           />
-          <button className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-">
+          <button className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0"
+          onClick={copyPasswordToClipboard}
+          >
             Copy
           </button>
         </div>
@@ -27,7 +58,8 @@ function App() {
               type="range"
               min={6}
               max={100}
-              value={length}
+              value = {length}
+              onChange={(e) => setLength(e.target.value) }
               className="cursor-pointer"
             />
             <label>Length : {length}</label>
@@ -39,7 +71,7 @@ function App() {
               type="checkbox"
               // defaultChecked={numberAllowed}
               id="numberInput"
-              // onChange={() => setNumberAllowed((prev) => !prev)}
+              onChange={()=> setNumberAllowed((prev) => !prev)}
             />
 
             <label>Numbers</label>
@@ -51,7 +83,7 @@ function App() {
               type="checkbox"
               // defaultChecked ={charAllowed}
               id="characterInput"
-              // onChange={() => setCharAllowed((prev) => !prev)}
+              onChange={() => setCharAllowed((prev)=> !(prev))}
             />
             <label htmlFor="characterInput">Characters</label>
           </div>
